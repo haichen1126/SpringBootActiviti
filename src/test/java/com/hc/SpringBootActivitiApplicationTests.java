8f6a9271cc5d09e10ca4ec65f6a244f78401ab7b
15818 <1581854528@qq.com>
2022-03-15 16:28:22 +0800

    demo工具

---
+++ src/test/java/com/hc/SpringBootActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
