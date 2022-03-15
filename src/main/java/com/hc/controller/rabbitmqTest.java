68b9688009a39bae2ad306827145949a3ecb7a05
15818 <1581854528@qq.com>
2022-03-15 16:30:42 +0800

    测试修改

--- src/main/java/com/hc/controller/rabbitmqTest.java
+++ src/main/java/com/hc/controller/rabbitmqTest.java
@@ -1,7 +1,6 @@
 package com.hc.controller;
 
 import com.hc.rabbitmq.RabbitMQConfig;
-import com.hc.rabbitmq.Sender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,20 +17,20 @@ public class rabbitmqTest {
     public void testRabbitmq() throws Exception {
 //        sender.send();
 //        sender.send("123");
-        for (int i = 0; i < 10000; i++) {
-            /*MessagePostProcessor messagePostProcessor=message -> {
-                MessageProperties messageProperties = message.getMessageProperties();
-//            设置编码
-                messageProperties.setContentEncoding("utf-8");
-//            设置过期时间10*1000毫秒
-                messageProperties.setExpiration("5000");
-                return message;
-            };*/
-            rabbitTemplate.convertAndSend(RabbitMQConfig.TOPIC_EXCHANGE_BOOT,
-                    "direct_queue1",
-                    i+"");
+//        for (int i = 0; i < 10000; i++) {
+//            /*MessagePostProcessor messagePostProcessor=message -> {
+//                MessageProperties messageProperties = message.getMessageProperties();
+////            设置编码
+//                messageProperties.setContentEncoding("utf-8");
+////            设置过期时间10*1000毫秒
+//                messageProperties.setExpiration("5000");
+//                return message;
+//            };*/
+//            rabbitTemplate.convertAndSend(RabbitMQConfig.TOPIC_EXCHANGE_BOOT,
+//                    "direct_queue1",
+//                    i+"");
 
-        }
+//        }
     }
 
 
